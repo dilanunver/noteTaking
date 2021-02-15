1b4d6c6ece6c1aa3fd5e6e5da8c04a5477d8f601
Dilan <dunver33@gmail.com>
2021-02-15 21:26:00 +0300

    addItem and renderItems

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const input = document.querySelector('.text');
+console.log(input)
+const form = document.querySelector('.posting')
 const addBtn = document.querySelector('.submit');
+console.log(addBtn)
+const flexText = document.querySelector('.flex-wrap')
+const flex = document.querySelectorAll('.flex')
+itemArray = []
 
 
-addBtn.addEventListener('submit', addItem)
+form.addEventListener('submit', addItem)
 
-// you should add form in html 
-function addItem(e){ 
-  e.preventDefault()
-  const element = e.curretTarget;
-  console.log(element)
+function addItem(e){
+  e.preventDefault();
+  const value = input.value;
+  const id = new Date().getTime().toString()
+  const items = {
+    dateID : id,
+    nameValue : value,
+  }
+  itemArray.push(items)
+  setBackToDefault()
+  flexText.innerHTML = ''
+  renderItems()
+}
+
+function renderItems(){
+  itemArray.forEach(function(item,index){
+    let element = document.createElement('div');
+    element.classList.add('flex')
+    element.innerHTML = `<div class="item">Note ${index + 1}</div>
+    <p>${item.nameValue}</p>
+    <button type="button" class="detail">View Detail</button>`
+    flexText.classList.add('flex-show')
+    flexText.appendChild(element)
+  })
+  
+}
+
+function setBackToDefault(){
+  input.value = '';
 }
