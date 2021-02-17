788d593b3472bd6617d8b32ced323bd5c16a3b17
Dilan <dunver33@gmail.com>
2021-02-17 17:51:47 +0300

    adding delete and edit button

--- app.js
+++ app.js
@@ -7,22 +7,43 @@ const flexText = document.querySelector('.flex-wrap')
 const flex = document.querySelectorAll('.flex');
 const modalSection = document.querySelector('.modal-section')
 itemArray = []
+let editing = false; 
+let editElements;
 
 
-form.addEventListener('submit', addItem)
+form.addEventListener('submit', chooseOption)
+
+function chooseOption(e){
+  e.preventDefault()
+  if(editing){
+    editItems(e)
+  } else {
+    addItem(e)
+  }
+  
+}
+function editItems(e){
+  e.preventDefault()
+  editElements.nameValue = input.value
+   editing = false;
+   addBtn.textContent = 'Add Note';
+   setBackToDefault()
+   renderItems();
+ }
+
 
 function addItem(e){
-  e.preventDefault();
-  const value = input.value;
+  e.preventDefault()
   const id = new Date().getTime().toString()
+  const value = input.value;
   const items = {
     dateID : id,
     nameValue : value,
   }
   itemArray.push(items)
   setBackToDefault()
-  flexText.innerHTML = ''
   renderItems()
+  
 }
 
 function renderItems(){
@@ -34,17 +55,24 @@ function renderItems(){
     element.setAttributeNode(attr)
     element.innerHTML = `<div class="item">Note ${index + 1}</div>
     <p>${item.nameValue}</p>
-    <button type="button" class="detail">View Detail</button>`
+    <button type="button" class="detail">View Detail</button>
+    <button type="button" class="delete"><i class="far fa-trash-alt"></i></button>
+    <button type="button" class="edit"><i class="far fa-edit"></i></button>`
     flexText.classList.add('flex-show')
     flexText.appendChild(element)
     const detailBtn = element.querySelector('.detail');
-    detailBtn.addEventListener('click', openDetail)
+    detailBtn.addEventListener('click', openDetail);
+    const deleteBtn = element.querySelector('.delete');
+    deleteBtn.addEventListener('click', deleteItems);
+    const editBtn = element.querySelector('.edit');
+    editBtn.addEventListener('click', editingItems)
   })
   
 }
 
 function setBackToDefault(){
   input.value = '';
+  flexText.innerHTML = ''
 }
 
 function openDetail(e){
@@ -69,3 +97,29 @@ function close(e){
   button.classList.remove('modal-show')
   
 }
+function deleteItems(e){
+    const element = e.currentTarget.parentElement;
+    console.log(element)
+    const id = element.dataset.id
+    console.log(id)
+    const deletedItems = itemArray.find(item =>{
+      return item.dateID === id
+    })
+    const indexOfItem = itemArray.indexOf(deletedItems);
+    itemArray.splice(indexOfItem, 1)
+    setBackToDefault()
+    renderItems()
+    
+}
+function editingItems(e){
+  let element = e.currentTarget.parentElement;
+  let id = element.dataset.id;
+  editElements = itemArray.find(item =>{
+    return item.dateID == id;
+  })
+  input.value = editElements.nameValue
+ 
+  editing = true;
+  addBtn.textContent = 'Edit'
+}
+
